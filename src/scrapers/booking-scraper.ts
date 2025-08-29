0886607ca3367a5cddbf55f47563e74176f65d87
IrinaCotici <irina.cotici@stonesouptech.com>
2025-08-29 17:06:19 +0300

    keep tab open

--- src/scrapers/booking-scraper.ts
+++ src/scrapers/booking-scraper.ts
@@ -1011,8 +1011,7 @@ export class BookingScraper extends BaseScraper {
       }
       
       // Close the new tab and switch back
-      await newPage.close();
-      await this.logInfo(`Closed reservation detail tab`);
+      // await newPage.close();
       return true;
 
     } catch (error) {
